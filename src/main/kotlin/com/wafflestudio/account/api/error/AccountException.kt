6026eb74758ea63f1566968b22abb18c3f894417
Davin <bdv111@snu.ac.kr>
2022-05-07 13:20:13 +0900

    ktlint

--- src/main/kotlin/com/wafflestudio/account/api/error/AccountException.kt
+++ src/main/kotlin/com/wafflestudio/account/api/error/AccountException.kt
@@ -1,6 +1,6 @@
 package com.wafflestudio.account.api.error
 
-open class AccountException(val errorType: ErrorType): RuntimeException(errorType.name)
+open class AccountException(val errorType: ErrorType) : RuntimeException(errorType.name)
 
 object UserInactiveException : AccountException(ErrorType.USER_INACTIVE)
 object EmailAlreadyExistsException : AccountException(ErrorType.EMAIL_ALREADY_EXISTS)
