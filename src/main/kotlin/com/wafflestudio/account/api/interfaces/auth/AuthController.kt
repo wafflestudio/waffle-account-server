c719721886edd8af8fbdf823754facd1b7da5c90
chosanglyul <chosanglyul@gmail.com>
2022-05-21 18:09:08 +0900

    Update AuthController.kt

--- src/main/kotlin/com/wafflestudio/account/api/interfaces/auth/AuthController.kt
+++ src/main/kotlin/com/wafflestudio/account/api/interfaces/auth/AuthController.kt
@@ -1,8 +1,8 @@
 package com.wafflestudio.account.api.interfaces.auth
 
-import org.springframework.web.bind.annotation.GetMapping
 import com.wafflestudio.account.api.security.CurrentUser
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
