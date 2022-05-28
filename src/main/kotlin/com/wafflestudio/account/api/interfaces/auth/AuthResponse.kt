919908b49597359457ef1a741d79684603b82ca5
chosanglyul <chosanglyul@gmail.com>
2022-05-28 13:54:30 +0900

    Add UserResponse

--- src/main/kotlin/com/wafflestudio/account/api/interfaces/auth/AuthResponse.kt
+++ src/main/kotlin/com/wafflestudio/account/api/interfaces/auth/AuthResponse.kt
@@ -15,6 +15,26 @@ data class RefreshResponse(
     val accessToken: String,
 )
 
+data class UserResponse(
+    @JsonProperty("user_id")
+    val userId: Long,
+
+    @JsonProperty("name")
+    val username: String?,
+
+    @JsonProperty("email")
+    val email: String?,
+
+    @JsonProperty("is_active")
+    val isActive: Boolean,
+
+    @JsonProperty("is_banned")
+    val isBanned: Boolean,
+
+    @JsonProperty("provider")
+    val provider: String,
+)
+
 data class UserIDResponse(
     @JsonProperty("user_id")
     val userId: Long,
