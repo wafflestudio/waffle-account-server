e2ad4562c8b8f59b309091e962ffa3fbea7fa7b7
Hank-Choi <zlzlqlzl1@naver.com>
2022-07-16 19:07:17 +0900

    Rest Docs 적용

---
+++ src/test/kotlin/com/wafflestudio/account/SampleTest.kt
@@ -0,0 +1,39 @@
+package com.wafflestudio.account
+
+import com.wafflestudio.account.api.interfaces.health.HealthCheckController
+import io.kotest.core.spec.style.WordSpec
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.restdocs.ManualRestDocumentation
+import org.springframework.restdocs.operation.preprocess.Preprocessors.*
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest
+class SampleTest(val healthCheckController: HealthCheckController) : WordSpec({
+    val restDocumentation = ManualRestDocumentation()
+
+    val webTestClient = WebTestClient.bindToController(healthCheckController)
+        .configureClient()
+        .filter(documentationConfiguration(restDocumentation))
+        .build()
+    beforeEach {
+        restDocumentation.beforeTest(javaClass, "SampleTest") // 2
+    }
+
+    afterEach {
+        restDocumentation.afterTest() // 2
+    }
+
+    "request health_check" should {
+        "response ok" {
+            webTestClient.get().uri("/health_check").exchange().expectStatus().isOk.expectBody().consumeWith(
+                document(
+                    "health",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())
+                )
+            )
+        }
+    }
+})
