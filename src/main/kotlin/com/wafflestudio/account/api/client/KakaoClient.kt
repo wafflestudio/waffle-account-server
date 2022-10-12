fb95e1aee93da8b73a1cbd50434832cc97d49a08
davin111 <bdv111@snu.ac.kr>
2022-10-13 02:54:43 +0900

    file formatting

--- src/main/kotlin/com/wafflestudio/account/api/client/KakaoClient.kt
+++ src/main/kotlin/com/wafflestudio/account/api/client/KakaoClient.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.account.api.client
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.wafflestudio.account.api.domain.account.enum.SocialProvider
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.http.MediaType
@@ -72,3 +73,17 @@ class KakaoClient(
         }
     }
 }
+
+data class KakaoOAuth2TokenResponse(
+    @JsonProperty("token_type")
+    val tokenType: String?,
+    @JsonProperty("access_token")
+    val accessToken: String,
+    @JsonProperty("expires_in")
+    val expiresIn: Int?,
+    @JsonProperty("refresh_token")
+    val refreshToken: String?,
+    @JsonProperty("refresh_token_expires_in")
+    val refreshTokenExpiresIn: Int?,
+    val scope: String?,
+)
