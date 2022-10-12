4cc1691649ac51386a6b2b73110293cd24126408
davin111 <bdv111@snu.ac.kr>
2022-10-13 02:56:14 +0900

    delete unused method

--- src/main/kotlin/com/wafflestudio/account/api/domain/account/UserRepository.kt
+++ src/main/kotlin/com/wafflestudio/account/api/domain/account/UserRepository.kt
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Repository
 @Repository
 interface UserRepository : CoroutineCrudRepository<User, Long> {
     suspend fun findByEmail(email: String): User?
-
     suspend fun findByProviderAndSocialId(provider: SocialProvider, socialId: String): User?
-    suspend fun existsByEmail(email: String): Boolean
     suspend fun findAllByIdIsIn(ids: List<Long>): List<User>
 }
