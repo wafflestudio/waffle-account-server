a7359438973bbb5bfda94ff863e3c5c1c83bd60f
SangLyul Cho <chosanglyul@gmail.com>
2022-11-26 20:56:15 +0900

    Update GithubClient.kt (#49)

--- src/main/kotlin/com/wafflestudio/account/api/client/GithubClient.kt
+++ src/main/kotlin/com/wafflestudio/account/api/client/GithubClient.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.account.api.client
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.wafflestudio.account.api.domain.account.enum.SocialProvider
 import com.wafflestudio.account.api.interfaces.auth.OAuth2RequestWithAuthCode
 import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.http.MediaType
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.bodyToMono
@@ -10,7 +12,7 @@ import reactor.core.publisher.Mono
 
 @Component("GITHUB")
 class GithubClient(
-    webClientHelper: WebClientHelper,
+    private val webClientHelper: WebClientHelper,
     clientRegistrationRepository: ReactiveClientRegistrationRepository,
 ) : OAuth2Client {
 
@@ -45,6 +47,38 @@ class GithubClient(
     override suspend fun getMeWithAuthCode(
         oAuth2RequestWithAuthCode: OAuth2RequestWithAuthCode
     ): OAuth2UserResponse? {
-        throw NotImplementedError()
+        val tokenResponse = webClient
+            .post()
+            .uri(clientRegistration.providerDetails.tokenUri)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .bodyValue(
+                webClientHelper.makeMultiValueMap(
+                    mapOf(
+                        "client_id" to clientRegistration.clientId,
+                        "client_secret" to clientRegistration.clientSecret,
+                        "code" to oAuth2RequestWithAuthCode.authorizationCode,
+                        "redirect_uri" to oAuth2RequestWithAuthCode.redirectUri,
+                    )
+                )
+            )
+            .retrieve()
+            .bodyToMono<GithubOAuth2TokenResponse>()
+            .onErrorResume {
+                WebClientHelper.logger.error(it.message, it)
+                Mono.empty()
+            }.awaitSingleOrNull()
+
+        return tokenResponse?.let {
+            getMe(it.accessToken)
+        }
     }
 }
+
+data class GithubOAuth2TokenResponse(
+    @JsonProperty("access_token")
+    val accessToken: String,
+    @JsonProperty("scope")
+    val scope: String,
+    @JsonProperty("token_type")
+    val tokenType: String,
+)
