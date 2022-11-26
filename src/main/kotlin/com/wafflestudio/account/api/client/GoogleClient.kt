6b68934603a59dd1cdc74f7184997f8c83457b00
Davin Byeon (Dan) <bdv111@snu.ac.kr>
2022-11-26 20:36:10 +0900

    google login (#48)

--- src/main/kotlin/com/wafflestudio/account/api/client/GoogleClient.kt
+++ src/main/kotlin/com/wafflestudio/account/api/client/GoogleClient.kt
@@ -1,8 +1,10 @@
 package com.wafflestudio.account.api.client
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.wafflestudio.account.api.domain.account.enum.SocialProvider
 import com.wafflestudio.account.api.interfaces.auth.OAuth2RequestWithAuthCode
 import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.http.MediaType
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.bodyToMono
@@ -10,7 +12,7 @@ import reactor.core.publisher.Mono
 
 @Component("GOOGLE")
 class GoogleClient(
-    webClientHelper: WebClientHelper,
+    private val webClientHelper: WebClientHelper,
     clientRegistrationRepository: ReactiveClientRegistrationRepository,
 ) : OAuth2Client {
 
@@ -44,8 +46,46 @@ class GoogleClient(
     }
 
     override suspend fun getMeWithAuthCode(
-        oAuth2RequestWithAuthCode: OAuth2RequestWithAuthCode
+        oAuth2RequestWithAuthCode: OAuth2RequestWithAuthCode,
     ): OAuth2UserResponse? {
-        throw NotImplementedError()
+        val tokenResponse = webClient
+            .post()
+            .uri(clientRegistration.providerDetails.tokenUri)
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .bodyValue(
+                webClientHelper.makeMultiValueMap(
+                    mapOf(
+                        "grant_type" to "authorization_code",
+                        "client_id" to clientRegistration.clientId,
+                        "client_secret" to clientRegistration.clientSecret,
+                        "redirect_uri" to oAuth2RequestWithAuthCode.redirectUri,
+                        "code" to oAuth2RequestWithAuthCode.authorizationCode,
+                    )
+                )
+            )
+            .retrieve()
+            .bodyToMono<GoogleOAuth2TokenResponse>()
+            .onErrorResume {
+                WebClientHelper.logger.error(it.message, it)
+                Mono.empty()
+            }.awaitSingleOrNull()
+
+        return tokenResponse?.let {
+            getMe(it.accessToken)
+        }
     }
 }
+
+data class GoogleOAuth2TokenResponse(
+    @JsonProperty("token_type")
+    val tokenType: String?,
+    @JsonProperty("access_token")
+    val accessToken: String,
+    @JsonProperty("expires_in")
+    val expiresIn: Int?,
+    @JsonProperty("refresh_token")
+    val refreshToken: String?,
+    @JsonProperty("refresh_token_expires_in")
+    val refreshTokenExpiresIn: Int?,
+    val scope: String?,
+)
