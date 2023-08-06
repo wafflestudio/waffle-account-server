a7d04f41350d1cccee66033ad3d15505cf3c7d87
davin111 <bdv111@snu.ac.kr>
2023-08-06 19:21:24 +0900

    print all headers in health_check api

--- src/main/kotlin/com/wafflestudio/account/api/interfaces/health/HealthCheckController.kt
+++ src/main/kotlin/com/wafflestudio/account/api/interfaces/health/HealthCheckController.kt
@@ -4,11 +4,19 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ServerWebExchange
 
 @RestController
 class HealthCheckController {
     @GetMapping("/health_check")
     @ResponseStatus(HttpStatus.OK)
-    suspend fun healthCheck() {
+    suspend fun healthCheck(
+        exchange: ServerWebExchange,
+    ): String {
+        val headersString = StringBuilder()
+        exchange.request.headers.forEach { key, value ->
+            headersString.append(key).append(": ").append(value).append("\n")
+        }
+        return headersString.toString()
     }
 }
